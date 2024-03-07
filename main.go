ec32f6f183526c329462f7e241bd77844ce31bc9
Peter Willis <peter.willis@cudoventures.com>
2024-03-07 12:02:14 +0000

    correct default host:port

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ var (
 	version           = "dev"
 	commit            = "unknown"
 	date              = "unknown"
-	defaultRemoteAddr = "grpc.staging.compute.cudo.org:443"
+	defaultRemoteAddr = "grpc.compute.cudo.org:443"
 )
 
 func main() {
