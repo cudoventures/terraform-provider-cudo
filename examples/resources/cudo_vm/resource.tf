6651766b328bc095ba50d8bc016b347722e3b076
Peter Willis <peter.willis@cudoventures.com>
2024-08-08 09:37:16 +0100

    update examples

--- examples/resources/cudo_vm/resource.tf
+++ examples/resources/cudo_vm/resource.tf
@@ -1,41 +1,24 @@
-# start a vm in any data center by specifying a maximum price
-resource "cudo_vm" "my-vm-max-price" {
-  id         = "terra-vm-1"
-  memory_gib = 2
-  vcpus      = 1
-  boot_disk = {
-    image_id = "debian-11"
-  }
-  networks = [
-    {
-      network_id         = "my-network"
-      assign_public_ip   = true
-      security_group_ids = ["my-security-group"]
-    }
-  ]
-}
-
 resource "cudo_storage_disk" "my-storage-disk" {
   data_center_id = "gb-bournemouth-1"
-  id             = "my-disk"
+  id             = "storage-disk-1"
   size_gib       = 100
 }
 
 # pick a specific data center and machine type
 resource "cudo_vm" "my-vm" {
   depends_on     = [cudo_storage_disk.my-storage-disk]
-  id             = "terra-vm-1"
-  machine_type   = "standard"
+  id             = "instance-1"
+  machine_type   = "intel-broadwell"
   data_center_id = "gb-bournemouth-1"
   memory_gib     = 2
   vcpus          = 1
   boot_disk = {
-    image_id = "debian-11"
-    size_gib = 50
+    image_id = "debian-12"
+    size_gib = 10
   }
   storage_disks = [
     {
-      disk_id = "my-disk"
+      disk_id = "storage-disk-1"
     }
   ]
   ssh_key_source = "project"
